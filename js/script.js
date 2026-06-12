dd9a9442d4f0a17ef8666a26dd99a3e0bcf528c5
Andrey Sidorkin <gholos@yandex.ru>
2026-06-12 15:43:56 +0700

    Add basic equal functional

--- js/script.js
+++ js/script.js
@@ -1,23 +1,70 @@
-let num1;
-let num2;
-let operator;
+const display = document.querySelector(".calc__display");
+const numBtns = document.querySelectorAll(".calc__numBtn");
+const clearBtn = document.querySelector(".calc__clearBtn");
+const equalsBtn = document.querySelector(".calc__eqBtn");
+const operBtns = document.querySelectorAll(".calc__operBtn");
 
-function operate(operator, num1, num2) {
+let num1 = '';
+let num2 = '';
+let operator = '';
 
-  switch (operator) {
-    case '+':
-      return add(num1, num2);
-    case '-':
-      return subtract(num1, num2);
-    case '*':
-      return multiply(num1, num2);
-    case '/':
-      return devide(num1, num2);
+numBtns.forEach((numBtn) => numBtn.addEventListener("click", () => {
+  if (operator === '') {
+    takeNum1(numBtn);
+  } else {
+    takeNum2(numBtn);
   }
+  console.log(num1, num2, operator);
+}));
 
+operBtns.forEach((operBtn) =>
+  operBtn.addEventListener("click", () => {
+    operator = operBtn.textContent;
+    clearDisplay();
+  }),
+);
+
+equalsBtn.addEventListener('click', () => {
+  display.textContent = operate(operator, +num1, +num2);
+});
+
+clearBtn.addEventListener("click", () => clearCalc());
+
+function takeNum1(numBtn) {
+  display.textContent += numBtn.textContent;
+  num1 = display.textContent;
 }
 
-function add(x,y) {
+function takeNum2(numBtn) {
+  display.textContent += numBtn.textContent;
+  num2 = display.textContent;
+}
+
+function clearCalc() {
+  num1 = '';
+  num2 = '';
+  operator = '';
+  display.textContent = "";
+}
+
+function clearDisplay() {
+  display.textContent = '';
+}
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return devide(num1, num2);
+  }
+}
+
+function add(x, y) {
   return x + y;
 }
 
