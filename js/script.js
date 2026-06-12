20075c832acd564e979ca6028e226cf0db6eb509
Andrey Sidorkin <gholos@yandex.ru>
2026-06-12 14:33:00 +0700

    Create base funcs

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function add(x,y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function devide(x, y) {
+  return x / y;
+}
+
+console.log(add(2,3));
+console.log(subtract(2,3));
+console.log(multiply(2,3));
+console.log(devide(2,3));
